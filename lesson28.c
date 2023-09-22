a5e4a9a51e911f8c109687e0ce78cc7ce34d0957
lyraaaaaaa <Lyraaaaaaaaaaa@gmail.com>
2023-09-22 13:41:45 +0800

    lesson28

--- lesson28.c
+++ lesson28.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+//打印二维数组
+int main()
+{
+    int arr1[3][4]={1,2,3,4,5,6,7,8,1,2,3,4};
+
+    int i;
+    for ( i=0 ; i<3 ; i++ )//i行
+    {
+        int j;
+        for( j=0 ; j<4 ; j++ )//j列
+
+        printf("%d\n",arr1[i][j]);
+    }
+    return 0;
+}
